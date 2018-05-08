3eb63c21704c26aa9a51cb1bbe692cfd61b62752
lyy603 <840567289@qq.com>
2018-05-08 15:18:31 +0800

    修改门牌信息列表界面

--- app/src/main/java/com/yaheen/pdaapp/activity/ManageActivity.java
+++ app/src/main/java/com/yaheen/pdaapp/activity/ManageActivity.java
@@ -2,6 +2,8 @@ package com.yaheen.pdaapp.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.yaheen.pdaapp.R;
@@ -11,9 +13,11 @@ import com.yaheen.pdaapp.bean.MsgBean;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManageActivity extends Activity {
+public class ManageActivity extends BaseActivity {
 
     private ListView listView;
+
+    private LinearLayout llBack;
 
     private ManageMsgAdapter msgAdapter;
 
@@ -22,11 +26,21 @@ public class ManageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
 
+        setTitleContent(R.string.title_bar_msg_text);
+
         listView = findViewById(R.id.lv_msg);
+        llBack = findViewById(R.id.back);
 
         msgAdapter = new ManageMsgAdapter(this);
         listView.setAdapter(msgAdapter);
         initLocalMsg();
+
+        llBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 
     private void initLocalMsg() {
@@ -42,5 +56,10 @@ public class ManageActivity extends Activity {
         beanList.add(bean);
         msgAdapter.setDatas(beanList);
         msgAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
